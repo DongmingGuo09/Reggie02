1213c32dad158b305bed24e6eb9a39481f89ce9c
Dongmingguo09 <Dongming.guo.99@gmail.com>
2023-03-04 11:15:08 +0800

    Adding front end files, Create Srping project start files

---
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -0,0 +1,16 @@
+package com.itheima.reggie;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class, args);
+        log.info("Project Starts successfully!");
+    }
+}
